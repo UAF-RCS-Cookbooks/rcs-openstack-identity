7db1fef0abcb4c2bbf3d138cf8e897431d37796b
Jeremiah Dabney <jdabney@gmail.com>
2024-07-18 17:26:37 -0800

    Migrating to RCS managed.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'openstack-identity'
-maintainer       'openstack-chef'
-maintainer_email 'openstack-discuss@lists.openstack.org'
+name             'rcs-openstack-identity'
+maintainer       'UAF RCS'
+maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'The OpenStack Identity service Keystone.'
 version          '20.0.0'
@@ -10,9 +10,7 @@ version          '20.0.0'
 end
 
 depends 'apache2', '~> 8.6'
-depends 'openstackclient'
-depends 'openstack-common', '>= 20.0.0'
+depends 'rcs-openstackclient'
+depends 'rcs-openstack-common', '>= 20.0.0'
 
-issues_url 'https://launchpad.net/openstack-chef'
-source_url 'https://opendev.org/openstack/cookbook-openstack-identity'
 chef_version '>= 16.0'
