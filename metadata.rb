83fefbe8be8db4336c33e002fe5aacda375b0873
Jeremiah Dabney <jdabney@gmail.com>
2025-04-09 22:10:44 -0800

    Bumped version.

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,13 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'The OpenStack Identity service Keystone.'
-version          '21.0.4'
+version          '21.0.5'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
-depends 'apache2', '~> 8.15.0'
+depends 'apache2', '~> 9.3.6'
 depends 'rcs-openstackclient'
 depends 'rcs-openstack-common'
 
